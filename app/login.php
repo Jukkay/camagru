1927357860ea3731b33f323e7be8a46aefe0834f
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-06-13 13:13:24 +0300

    removed profile and control panel icons for unlogged users.

--- app/login.php
+++ app/login.php
@@ -5,7 +5,7 @@
 				<div class="field">
 					<label class="label">Username</label>
 					<div class="control has-icons-left has-icons-right">
-						<input class="input" type="text" id="username" name="username" placeholder="Username" minlength="3" maxlength="32" required>
+						<input class="input" type="text" autocomplete="username" id="username" name="username" placeholder="Username" minlength="3" maxlength="32" required>
 						<span class="icon is-small is-left">
 							<svg style="width:24px;height:24px" viewBox="0 0 24 24">
 							<path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
@@ -17,7 +17,7 @@
 				<div class="field">
 					<label class="label">Password</label>
 					<div class="control has-icons-left has-icons-right">
-						<input class="input" type="password" id="password" name="password" placeholder="Password" minlength="8" maxlength="255" required>
+						<input class="input" type="password" autocomplete="current-password" id="password" name="password" placeholder="Password" minlength="8" maxlength="255" required>
 						<span class="icon is-small is-left">
 							<svg style="width:24px;height:24px" viewBox="0 0 24 24">
 							<path fill="currentColor" d="M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z" />
