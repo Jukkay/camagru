bd431c9e8a5a8ccf0b31c0d9be78bccb12781249
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-06-15 12:30:26 +0300

    preliminary fix for hamburger menu not working

---
+++ app/public/js/header.js
@@ -0,0 +1,15 @@
+let hamburger = document.getElementsByClassName('navbar-burger')[0];
+let mobile = document.getElementById('navbar-mobile');
+
+hamburger.addEventListener('click', () => {
+	if (hamburger.classList.contains('is-active')) {
+		hamburger.classList.remove('is-active');
+		mobile.classList.remove('is-active');
+		mobile.classList.add('is-hidden');
+	}
+	else {
+		hamburger.classList.add('is-active');
+		mobile.classList.add('is-active');
+		mobile.classList.remove('is-hidden');
+	}
+})
