7effc5fe4b4c6d765e4a2a07b4e404b7550a4bbb
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-06-17 12:04:14 +0300

    made db passwords secret

--- app/config/database.php
+++ app/config/database.php
@@ -2,18 +2,19 @@
 
 $DB_NAME = 'camagru';
 $DB_USER = 'camagru';
-$DB_PASSWORD = 'camagru';
+$DB_PASSWORD = rtrim(file_get_contents("/run/secrets/mysql_password"));
 $HOSTNAME = 'mariadb';
 $DB_DSN = 'mysql:host=' . $HOSTNAME . ';dbname=' . $DB_NAME . ';charset=UTF8';
 
 class Database {
 	protected $DB_NAME = 'camagru';
 	protected $DB_USER = 'camagru';
-	protected $DB_PASSWORD = 'camagru';
+	protected $DB_PASSWORD;
 	protected $HOSTNAME = 'mariadb';
 	protected $DB_DSN;
 
 	public function __construct () {
+		$this->DB_PASSWORD = rtrim(file_get_contents("/run/secrets/mysql_password"));
 		$this->DB_DSN = 'mysql:host=' . $this->HOSTNAME . ';dbname=' . $this->DB_NAME;
 	}
 }
